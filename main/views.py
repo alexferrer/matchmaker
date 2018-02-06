8af23cae2dcdaf8b92f8e55b7d6137aae83d3376
Alex Ferrer <ferrerdallas@gmail.com>
2018-02-05 20:42:12 -0600

    Manual forms

--- main/views.py
+++ main/views.py
@@ -1,6 +1,15 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.shortcuts import render
+from .models import Trade
+from django.views.generic.edit import CreateView
 
-# Create your views here.
+
+class TradeForm(CreateView):
+    model = Trade
+    fields = ['type_of', 'qty', 'price']
+    success_url = '/trade/create/'
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(TradeForm, self).form_valid(form)
